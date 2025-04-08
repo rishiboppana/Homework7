cf8298919e956927e2220d4ec16a6b1c592c6e66
rishiboppana <129854970+rishiboppana@users.noreply.github.com>
2025-04-08 12:07:24 -0700

    session_timestamp.sql

--- homework7/models/input/session_timestamp.sql
+++ homework7/models/input/session_timestamp.sql
@@ -1,5 +1,6 @@
-SELECT
- sessionId,
- ts
+with session_timestamp_cte as (SELECT sessionId,ts
 FROM {{ source('raw', 'session_timestamp') }}
 WHERE sessionId IS NOT NULL
+)
+
+select * from session_timestamp_cte
