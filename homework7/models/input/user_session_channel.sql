f5de9a64a984ee0fb4fa99b3b49408af90c8dd61
rishiboppana <129854970+rishiboppana@users.noreply.github.com>
2025-04-08 18:54:48 -0700

    user_session_channel.sql

--- homework7/models/input/user_session_channel.sql
+++ homework7/models/input/user_session_channel.sql
@@ -1,7 +1,3 @@
-with user_session_channel_cte as (
 SELECT userId,sessionId,channel
 FROM {{ source('raw', 'user_session_channel') }}
 WHERE sessionId IS NOT NULL
-)
-
-select * from user_session_channel_cte
